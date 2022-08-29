7cd7d6e7f0a48eb4fae3e360e98165bf25346dac
Caleb <calebwagner72@yahoo.com>
2022-08-29 08:22:45 -0500

    create state manager file

---
+++ service/stateManager.js
@@ -0,0 +1,18 @@
+const baseUrl = "http://localhost:8088";
+
+const appState = {
+    categories: [],
+    connections: [],
+    currentUser: {},
+    meetings: [],
+    messages: [],
+    posts: [],
+    savedPosts: [],
+    users: [],
+    postModal: false,
+    categoryModal: false,
+    meetingModal: false,
+    messageModal: false
+}
+
+const mainEl = document.getElementById(".DOM");
